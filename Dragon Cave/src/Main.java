9c22dd1944dfc6a9fe403395724a493f1a1e331e
Kristian <codingwithak222@gmail.com>
2022-03-29 15:59:11 -0400

    fixed scanner bug looking for string instead of int

--- Dragon Cave/src/Main.java
+++ Dragon Cave/src/Main.java
@@ -10,16 +10,16 @@ public class Main {
         System.out.println(" In one cave, the dragon is friendly and will share his treasure with you.");
         System.out.println("The other dragon is greedy and hungry and will eat you on sight.");
         System.out.println("Which cave will you go into? (1 or 2)");
-        int input = getInput.nextLine();
+         int input = getInput.nextInt();
 
         if (input == 1){
-            System.out.println("You approuach the cave...");
+            System.out.println("You approach the cave...");
             System.out.println("It is dark and spooky...");
             System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
             System.out.println("Gobbles you down in one bite!");
         }
         else if (input == 2){
-            System.out.println("You approuach the cave...");
+            System.out.println("You approach the cave...");
             System.out.println("It is dark and spooky...");
             System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
             System.out.println("A mountain of gold pours out!!!");
