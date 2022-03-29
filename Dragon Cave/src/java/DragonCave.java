1d1cd8f7928433943acc3c7a31bbcaa1b97a4b7d
Kristian <codingwithak222@gmail.com>
2022-03-29 17:11:08 -0400

    refactored code to make easier to test

--- Dragon Cave/src/java/DragonCave.java
+++ Dragon Cave/src/java/DragonCave.java
@@ -1,30 +1,46 @@
-
 import java.util.Scanner;
 
 public class DragonCave {
-    public static void main(String[] args) {
-        // need to add unit tests still
-        Scanner getInput = new Scanner(System.in);
 
+    int cave;
+    public void beginStory(){
         System.out.println("You are in a land full of dragons.");
         System.out.println("In front of you, you see two caves.");
         System.out.println(" In one cave, the dragon is friendly and will share his treasure with you.");
         System.out.println("The other dragon is greedy and hungry and will eat you on sight.");
         System.out.println("Which cave will you go into? (1 or 2)");
-         int input = getInput.nextInt();
 
-        if (input == 1){
+        getCave();
+
+    }
+
+     public int getCave(){
+        Scanner getInput = new Scanner(System.in);
+        this.cave = getInput.nextInt();
+        finishStory(this.cave);
+
+        return 0;
+
+    }
+
+     public  void finishStory(int cave){
+        if (this.cave == 1){
             System.out.println("You approach the cave...");
             System.out.println("It is dark and spooky...");
             System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
             System.out.println("Gobbles you down in one bite!");
         }
-        else if (input == 2){
+        else if (this.cave == 2){
             System.out.println("You approach the cave...");
             System.out.println("It is dark and spooky...");
             System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
             System.out.println("A mountain of gold pours out!!!");
             System.out.println("You take half and leave the cave unscathed.");
         }
+
     }
+     public static void main(String[] args){
+        new DragonCave().beginStory();
+     }
+
 }
---
+++ Dragon Cave/src/test/DragonCaveTest.java
@@ -0,0 +1,20 @@
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DragonCaveTest {
+
+    DragonCave dragonCave;
+    @BeforeEach
+    void setUp() {
+        dragonCave = new DragonCave();
+        // create a new instance object before every test
+    }
+    @Test
+    void main() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
