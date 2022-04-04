0e79ff221c6ab41f2e1511e88ebab298a36d9e71
Kristian <codingwithak222@gmail.com>
2022-04-03 21:51:39 -0400

    added unit test and exceptions

--- Dragon Cave/src/java/DragonCave.java
+++ Dragon Cave/src/java/DragonCave.java
@@ -2,45 +2,60 @@ import java.util.Scanner;
 
 public class DragonCave {
 
-    int cave;
-    public void beginStory(){
+    public static void main(String[] args){
+        int cave = 0;
+        Scanner getInput = new Scanner(System.in);
+        beginStory();
+        try {
+            cave = getInput.nextInt();
+        } catch (Exception e) {
+            System.out.println("ERROR:Run program again and make sure your selection is 1 or 2");
+        }
+        getCave(cave);
+        getInput.close();
+    }
+
+    public static void beginStory(){
         System.out.println("You are in a land full of dragons.");
         System.out.println("In front of you, you see two caves.");
         System.out.println(" In one cave, the dragon is friendly and will share his treasure with you.");
         System.out.println("The other dragon is greedy and hungry and will eat you on sight.");
         System.out.println("Which cave will you go into? (1 or 2)");
 
-        getCave();
+
 
     }
 
-     public int getCave(){
-        Scanner getInput = new Scanner(System.in);
-        this.cave = getInput.nextInt();
-        finishStory(this.cave);
+     public static int getCave(int cave){
+
+
+        finishStory(cave);
 
         return 0;
 
     }
 
-     public  void finishStory(int cave){
-        if (this.cave == 1){
+     public static void finishStory(int cave){
+        if (cave == 1){
             System.out.println("You approach the cave...");
             System.out.println("It is dark and spooky...");
             System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
             System.out.println("Gobbles you down in one bite!");
         }
-        else if (this.cave == 2){
+        else if (cave == 2){
             System.out.println("You approach the cave...");
             System.out.println("It is dark and spooky...");
             System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
             System.out.println("A mountain of gold pours out!!!");
             System.out.println("You take half and leave the cave unscathed.");
         }
+        else{
+            System.out.println("You chose to not enter the cave. Wise choice...");
+        }
+
 
     }
-     public static void main(String[] args){
-        new DragonCave().beginStory();
-     }
+
+
 
 }
--- Dragon Cave/src/test/DragonCaveTest.java
+++ Dragon Cave/src/test/DragonCaveTest.java
@@ -1,20 +1,86 @@
 
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 class DragonCaveTest {
 
-    DragonCave dragonCave;
+    DragonCave dragonCaveTest;
     @BeforeEach
     void setUp() {
-        dragonCave = new DragonCave();
-        // create a new instance object before every test
-    }
-    @Test
-    void main() {
     }
 
     @AfterEach
     void tearDown() {
     }
+    @Test
+    void testOption1() {
+
+        String expected = "You approach the cave...\n" +
+                "It is dark and spooky...\n" +
+                "A large dragon jumps out in front of you!  He opens his jaws and...\n" +
+                "Gobbles you down in one bite!\n";
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos);
+        System.setOut(printStream);
+
+        DragonCave.getCave(1);
+
+        String[] lines = baos.toString().split(System.lineSeparator());
+        String actual = lines[lines.length - 1];
+
+        assertEquals(expected, actual, "Error with user input: 1");
+    }
+
+    @Test
+    void testOption2() {
+
+        String expected = "You approach the cave...\n" +
+                "The entrance seems inviting enough as you cautiously peak inside...\n" +
+                "Inside the cave you find glittering gold and sparkling treasures!\n" +
+                "A golden nimbus of light appears suddenly and coalesces into the form of a giant dragon!\n" +
+                "The dragon generously shares his wealth with you!\n" +
+                "At dinner time, it gobbles you up because... dragon!\n";
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos);
+        System.setOut(printStream);
+
+        DragonCave.getCave(2);
+
+        String[] lines = baos.toString().split(System.lineSeparator());
+        String actual = lines[lines.length - 1];
+
+        assertEquals(expected, actual, "Error with user input: 2");
+    }
+
+
+    @Test
+    void testInvalidUserInput() {
+
+        //option to be tested
+        String option = "53288iier";
+        String expected = "Not a valid choice.\n";
+
+        //simulates user input
+        String userInput = String.format(option, System.lineSeparator(), System.lineSeparator());
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(bais);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos);
+        System.setOut(printStream);
+
+        DragonCave.main(null);
+
+        String[] lines = baos.toString().split(System.lineSeparator());
+        String actual = lines[lines.length - 1];
+
+        assertEquals(expected, actual);
+    }
+
+
+
 }
