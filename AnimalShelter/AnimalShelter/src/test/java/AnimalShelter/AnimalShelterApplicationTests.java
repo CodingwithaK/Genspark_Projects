064bb3c6a7fed04aec184482de314a329e6f2dc1
Kristian <codingwithak222@gmail.com>
2022-05-23 12:54:07 -0400

    set up project

---
+++ AnimalShelter/AnimalShelter/src/test/java/AnimalShelter/AnimalShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package AnimalShelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
